2a3851724f0d8198065ca5f9cda8d7f2f2fd03c7
Resery <50428593+Resery@users.noreply.github.com>
2020-07-06 18:58:39 +0800

    Create type_traits.h

---
+++ weak 1-2/Resery_STL/type_traits.h
@@ -0,0 +1,234 @@
+#pragma once
+#include "stl_config.h"
+
+__STL_BEGIN_NAMESPACE
+
+struct __true_type {
+};
+
+struct __false_type {
+};
+
+//定义模板(泛化)
+template <class _Tp>
+struct __type_traits {
+	typedef __true_type     this_dummy_member_must_be_first;
+	typedef __false_type    has_trivial_default_constructor;
+	typedef __false_type    has_trivial_copy_constructor;
+	typedef __false_type    has_trivial_assignment_operator;
+	typedef __false_type    has_trivial_destructor;
+	typedef __false_type    is_POD_type;
+};
+
+//模板特化，当传进来的是布尔型该怎么做，下面都是一样的
+__STL_TEMPLATE_NULL struct __type_traits<bool> {
+	typedef __true_type    has_trivial_default_constructor;
+	typedef __true_type    has_trivial_copy_constructor;
+	typedef __true_type    has_trivial_assignment_operator;
+	typedef __true_type    has_trivial_destructor;
+	typedef __true_type    is_POD_type;
+};
+
+
+__STL_TEMPLATE_NULL struct __type_traits<char> {
+	typedef __true_type    has_trivial_default_constructor;
+	typedef __true_type    has_trivial_copy_constructor;
+	typedef __true_type    has_trivial_assignment_operator;
+	typedef __true_type    has_trivial_destructor;
+	typedef __true_type    is_POD_type;
+};
+
+__STL_TEMPLATE_NULL struct __type_traits<signed char> {
+	typedef __true_type    has_trivial_default_constructor;
+	typedef __true_type    has_trivial_copy_constructor;
+	typedef __true_type    has_trivial_assignment_operator;
+	typedef __true_type    has_trivial_destructor;
+	typedef __true_type    is_POD_type;
+};
+
+__STL_TEMPLATE_NULL struct __type_traits<unsigned char> {
+	typedef __true_type    has_trivial_default_constructor;
+	typedef __true_type    has_trivial_copy_constructor;
+	typedef __true_type    has_trivial_assignment_operator;
+	typedef __true_type    has_trivial_destructor;
+	typedef __true_type    is_POD_type;
+};
+
+
+__STL_TEMPLATE_NULL struct __type_traits<short> {
+	typedef __true_type    has_trivial_default_constructor;
+	typedef __true_type    has_trivial_copy_constructor;
+	typedef __true_type    has_trivial_assignment_operator;
+	typedef __true_type    has_trivial_destructor;
+	typedef __true_type    is_POD_type;
+};
+
+__STL_TEMPLATE_NULL struct __type_traits<unsigned short> {
+	typedef __true_type    has_trivial_default_constructor;
+	typedef __true_type    has_trivial_copy_constructor;
+	typedef __true_type    has_trivial_assignment_operator;
+	typedef __true_type    has_trivial_destructor;
+	typedef __true_type    is_POD_type;
+};
+
+__STL_TEMPLATE_NULL struct __type_traits<int> {
+	typedef __true_type    has_trivial_default_constructor;
+	typedef __true_type    has_trivial_copy_constructor;
+	typedef __true_type    has_trivial_assignment_operator;
+	typedef __true_type    has_trivial_destructor;
+	typedef __true_type    is_POD_type;
+};
+
+__STL_TEMPLATE_NULL struct __type_traits<unsigned int> {
+	typedef __true_type    has_trivial_default_constructor;
+	typedef __true_type    has_trivial_copy_constructor;
+	typedef __true_type    has_trivial_assignment_operator;
+	typedef __true_type    has_trivial_destructor;
+	typedef __true_type    is_POD_type;
+};
+
+__STL_TEMPLATE_NULL struct __type_traits<long> {
+	typedef __true_type    has_trivial_default_constructor;
+	typedef __true_type    has_trivial_copy_constructor;
+	typedef __true_type    has_trivial_assignment_operator;
+	typedef __true_type    has_trivial_destructor;
+	typedef __true_type    is_POD_type;
+};
+
+__STL_TEMPLATE_NULL struct __type_traits<unsigned long> {
+	typedef __true_type    has_trivial_default_constructor;
+	typedef __true_type    has_trivial_copy_constructor;
+	typedef __true_type    has_trivial_assignment_operator;
+	typedef __true_type    has_trivial_destructor;
+	typedef __true_type    is_POD_type;
+};
+
+
+__STL_TEMPLATE_NULL struct __type_traits<float> {
+	typedef __true_type    has_trivial_default_constructor;
+	typedef __true_type    has_trivial_copy_constructor;
+	typedef __true_type    has_trivial_assignment_operator;
+	typedef __true_type    has_trivial_destructor;
+	typedef __true_type    is_POD_type;
+};
+
+__STL_TEMPLATE_NULL struct __type_traits<double> {
+	typedef __true_type    has_trivial_default_constructor;
+	typedef __true_type    has_trivial_copy_constructor;
+	typedef __true_type    has_trivial_assignment_operator;
+	typedef __true_type    has_trivial_destructor;
+	typedef __true_type    is_POD_type;
+};
+
+__STL_TEMPLATE_NULL struct __type_traits<long double> {
+	typedef __true_type    has_trivial_default_constructor;
+	typedef __true_type    has_trivial_copy_constructor;
+	typedef __true_type    has_trivial_assignment_operator;
+	typedef __true_type    has_trivial_destructor;
+	typedef __true_type    is_POD_type;
+};
+
+__STL_TEMPLATE_NULL struct __type_traits<char*> {
+	typedef __true_type    has_trivial_default_constructor;
+	typedef __true_type    has_trivial_copy_constructor;
+	typedef __true_type    has_trivial_assignment_operator;
+	typedef __true_type    has_trivial_destructor;
+	typedef __true_type    is_POD_type;
+};
+
+__STL_TEMPLATE_NULL struct __type_traits<signed char*> {
+	typedef __true_type    has_trivial_default_constructor;
+	typedef __true_type    has_trivial_copy_constructor;
+	typedef __true_type    has_trivial_assignment_operator;
+	typedef __true_type    has_trivial_destructor;
+	typedef __true_type    is_POD_type;
+};
+
+__STL_TEMPLATE_NULL struct __type_traits<unsigned char*> {
+	typedef __true_type    has_trivial_default_constructor;
+	typedef __true_type    has_trivial_copy_constructor;
+	typedef __true_type    has_trivial_assignment_operator;
+	typedef __true_type    has_trivial_destructor;
+	typedef __true_type    is_POD_type;
+};
+
+__STL_TEMPLATE_NULL struct __type_traits<const char*> {
+	typedef __true_type    has_trivial_default_constructor;
+	typedef __true_type    has_trivial_copy_constructor;
+	typedef __true_type    has_trivial_assignment_operator;
+	typedef __true_type    has_trivial_destructor;
+	typedef __true_type    is_POD_type;
+};
+
+__STL_TEMPLATE_NULL struct __type_traits<const signed char*> {
+	typedef __true_type    has_trivial_default_constructor;
+	typedef __true_type    has_trivial_copy_constructor;
+	typedef __true_type    has_trivial_assignment_operator;
+	typedef __true_type    has_trivial_destructor;
+	typedef __true_type    is_POD_type;
+};
+
+__STL_TEMPLATE_NULL struct __type_traits<const unsigned char*> {
+	typedef __true_type    has_trivial_default_constructor;
+	typedef __true_type    has_trivial_copy_constructor;
+	typedef __true_type    has_trivial_assignment_operator;
+	typedef __true_type    has_trivial_destructor;
+	typedef __true_type    is_POD_type;
+};
+
+//偏特化，对于原生指针应该怎么样
+template <class _Tp>
+struct __type_traits<_Tp*> {
+	typedef __true_type    has_trivial_default_constructor;
+	typedef __true_type    has_trivial_copy_constructor;
+	typedef __true_type    has_trivial_assignment_operator;
+	typedef __true_type    has_trivial_destructor;
+	typedef __true_type    is_POD_type;
+};
+
+//定义另一个模板，这个模板具体是干什么用的还不太清楚，根据给的注释(注释：我们单独进行此操作以减少依赖项的数量)，等到后面写完了，看到有对应的调用的地方再更新
+template <class _Tp> struct _Is_integer {
+	typedef __false_type _Integral;
+};
+
+__STL_TEMPLATE_NULL struct _Is_integer<bool> {
+	typedef __true_type _Integral;
+};
+
+__STL_TEMPLATE_NULL struct _Is_integer<char> {
+	typedef __true_type _Integral;
+};
+
+__STL_TEMPLATE_NULL struct _Is_integer<signed char> {
+	typedef __true_type _Integral;
+};
+
+__STL_TEMPLATE_NULL struct _Is_integer<unsigned char> {
+	typedef __true_type _Integral;
+};
+
+__STL_TEMPLATE_NULL struct _Is_integer<short> {
+	typedef __true_type _Integral;
+};
+
+__STL_TEMPLATE_NULL struct _Is_integer<unsigned short> {
+	typedef __true_type _Integral;
+};
+
+__STL_TEMPLATE_NULL struct _Is_integer<int> {
+	typedef __true_type _Integral;
+};
+
+__STL_TEMPLATE_NULL struct _Is_integer<unsigned int> {
+	typedef __true_type _Integral;
+};
+
+__STL_TEMPLATE_NULL struct _Is_integer<long> {
+	typedef __true_type _Integral;
+};
+
+__STL_TEMPLATE_NULL struct _Is_integer<unsigned long> {
+	typedef __true_type _Integral;
+};
+
+__STL_END_NAMESPACE
