851c570110fe5fded0c77c8c3eb19b23dd800795
Resery <50428593+Resery@users.noreply.github.com>
2020-07-06 18:56:31 +0800

    Create stl_config.h

---
+++ weak 1-2/Resery_STL/stl_config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifndef Resery_STL_STL_CONFIG_H
+#define Resery_STL_STL_CONFIG_H
+
+#define __STL_BEGIN_NAMESPACE namespace Resery_STL {
+#define __STL_END_NAMESPACE }
+
+
+#endif //Resery_STL_STL_CONFIG_H
